3fd86cd0e546cd6627d6a6a237f71a0737584e5c
skipper <skipper@aidigger.com>
2018-03-28 20:18:57 +0800

    can use

---
+++ center.go
@@ -0,0 +1,96 @@
+package pan
+
+import (
+	consulapi "github.com/hashicorp/consul/api"
+	"log"
+	"time"
+	"strings"
+	"encoding/json"
+)
+
+// read data from consul kv and watch it
+
+type CenterConfig struct {
+	Address 			string
+	Scheme 				string
+	Namespace 			string
+	NamespaceMap 	 	string   //string map for generate the key path. like "/Configs/{namespace}/{key}"
+	ContentType 		string   //type of consul value,  default json
+
+	Key 				string
+	keyIndex 			uint64
+	watch 				bool   //default watch  true
+
+}
+
+func ConsulKV(config CenterConfig) *consulapi.KV {
+	cconf := consulapi.DefaultConfig()
+	cconf.Address = config.Address
+	cconf.Scheme = config.Scheme
+	client, err := consulapi.NewClient(cconf)
+	if err != nil{
+		log.Panicf("Error creating consul client", err)
+	}
+	kv := client.KV()
+	return kv
+}
+
+func KVGet(key string, kv *consulapi.KV)([]byte,uint64){
+	pair,meta,err := kv.Get(key,nil)
+	if err != nil {
+		log.Panicln("Error read from KV", err.Error(), err)
+	}
+	return pair.Value,meta.LastIndex
+}
+
+func WatchKey(key string, ch chan []byte, kv *consulapi.KV, keyIndex uint64) {
+	currentIndex := keyIndex
+	for {
+
+		pair, meta, err := kv.Get(key, &consulapi.QueryOptions{
+			WaitIndex: currentIndex,
+		})
+		if err != nil {
+			log.Panicln("Error read from KV", err.Error(), err)
+		}
+
+		if pair == nil || meta == nil {
+			// Query won't be blocked if key not found
+			time.Sleep(3 * time.Second)
+		} else {
+			ch <- pair.Value
+			currentIndex = meta.LastIndex
+		}
+
+	}
+}
+
+func (p *Pan) ReadCenterWithWatch()  {
+
+	kv := ConsulKV(p.CenterConfig)
+	ch := make(chan []byte)
+	key := Sformat(p.CenterConfig.NamespaceMap,"{namespace}",p.CenterConfig.Namespace, "{key}",p.CenterConfig.Key)
+	centerMap := make(map[string]interface{})
+
+	//first get key from center
+	data,i1 := KVGet(key,kv)
+	json.Unmarshal(data, &centerMap)
+	p.center = UpMapKey(&centerMap)
+
+	// watch
+	go WatchKey(key,ch,kv,i1)
+	go func() {
+		for data := range ch {
+			json.Unmarshal(data, &centerMap)
+			p.center = UpMapKey(&centerMap)
+		}
+	}()
+
+
+}
+
+func Sformat(str string, args ...string) string {
+	r := strings.NewReplacer(args...)
+	ss := r.Replace(str)
+	return ss
+}
--- pan.go
+++ pan.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"fmt"
+	"strings"
+	"os"
+	"github.com/spf13/cast"
 )
 
 // thank you for viper by spf13
@@ -17,12 +20,26 @@ import (
 
 type Pan struct {
 
+	// Delimiter that separates a list of keys
+	// used to access a nested value in one go
+	keyDelim string
+
+
 	configFile 		string //config path with file name
 	configType 		string //config type json or yaml
+	envPrefix 		string
 
-	config map[string]interface{}
+	CenterConfig    CenterConfig
 
+	config 			map[string]interface{}
+	env 			map[string]interface{}
+	flag 			map[string]interface{} //Todo flag
+	center 			map[string]interface{}
+	override 		map[string]interface{}
+
+	typeByDefValue  bool
 }
+
 
 var p *Pan
 
@@ -33,10 +50,23 @@ func init()  {
 
 func New() *Pan  {
 	p := new(Pan)
+	p.keyDelim = "."
 	p.config = make(map[string]interface{})
+	p.env = make(map[string]interface{})
+	p.center = make(map[string]interface{})
+	p.override = make(map[string]interface{})
+	p.typeByDefValue = true
+	// set for Eigen
+	//p.CenterConfig.Address = "consul.aidigger.com:8500"
+	p.CenterConfig.Address = "192.168.3.14:8500"
+	p.CenterConfig.Scheme = "http"
+	p.CenterConfig.Namespace = "debug"
+	p.CenterConfig.NamespaceMap = "/Configs/{namespace}/{key}"
+
 	return p
 }
 
+// set config file
 
 func (p *Pan) SetConfigFile(path string, fileType string)  {
 	if path != "" {
@@ -64,13 +94,146 @@ func (p *Pan)ReadInConfig() error {
 	case "json":
 		json.Unmarshal(data,&conf)
 	case "yaml":
-		//TODO
+		//TODO support yaml
 		fmt.Println("jok")
 	}
-	p.config = conf
+	for k,_ := range conf {
+		k = strings.ToUpper(k)
+	}
+	p.config = UpMapKey(&conf)
 	return nil
 }
 
 func (p *Pan) Get(key string) interface{}  {
-	return p.config[key]
+	key = strings.ToUpper(key)
+	res := p.find(key)
+
+	// default json num type is float64
+	if p.typeByDefValue{
+		switch res.(type) {
+		case string:
+			return cast.ToString(res)
+		case int64:
+			return cast.ToInt64(res)
+		case map[string]interface{}:
+			return cast.ToStringMap(res)
+		case int32:
+			return cast.ToInt32(res)
+		case int8:
+			return cast.ToInt8(res)
+		case int:
+			return cast.ToInt(res)
+		case float64:
+			return cast.ToFloat64(res)
+		case float32:
+			return cast.ToFloat32(res)
+		}
+	}
+	return res
+}
+
+// env
+
+func (p *Pan)SetEnvPrefix(prefix string)  {
+	if prefix != ""{
+		p.envPrefix = prefix
+	}
+}
+
+func (p *Pan) ReadAllEnv()  {
+	envs := make(map[string]interface{})
+	sysEnvlist := os.Environ()
+	for _,e := range sysEnvlist{
+		tempList := strings.Split(e,"=")
+		envs[strings.ToUpper(tempList[0])] = tempList[1]
+	}
+	p.env = envs
+}
+
+func (p *Pan) ReadEnvWithPrefix()  {
+	//Todo readEnvWithPrefix
+}
+
+func UpMapKey(m *map[string]interface{}) map[string]interface{}  {
+
+	tempMap := make(map[string]interface{})
+	for k,v := range *m{
+		tempMap[strings.ToUpper(k)] = v
+	}
+
+	return tempMap
+}
+// search map
+
+//todo
+func (p *Pan) searchMap(source map[string]interface{}, path []string) interface{}  {
+
+	if len(path) == 0 {
+		return source
+	}
+	next,ok := source[path[0]]
+	
+	if ok {
+		// fast path
+		if len(path) == 1 {
+			return next
+		}
+		
+		//nested case
+
+		switch next.(type) {
+		case map[string]interface{}:
+			fmt.Println("the Map:",next.(map[string]interface{}))
+
+			newNext := cast.ToStringMap(next)
+			fmt.Println("New next:",next)
+			return p.searchMap(UpMapKey(&newNext), path[1:])
+		default:
+			fmt.Println(next)
+			return nil
+		}
+	}
+	
+	return nil
+}
+
+
+// Given a key, find the value.
+// Pan will check in the following order:
+// overwrite center env config
+// Note: this assumes a up-cased key given.
+
+func (p *Pan) find(key string) interface{}  {
+
+	var(
+		val interface{}
+		path = strings.Split(key, p.keyDelim)
+		//nested = len(path) > 1
+		)
+
+	fmt.Println("path:" ,path)
+	// override
+
+	val = p.searchMap(p.override,path)
+	if val != nil{
+		return val
+	}
+	//center
+
+	val = p.searchMap(p.center,path)
+	if val != nil{
+		return val
+	}
+	//env
+	val = p.searchMap(p.env,path)
+	if val != nil{
+		return val
+	}
+	// config
+	val = p.searchMap(p.config,path)
+	if val != nil{
+		return val
+	}
+
+	return nil
 }
